1ab99c4856069e957a1ef7c42e7e9377843064f9
arthurvenicio <arthur@justcode.dev>
2023-10-19 18:59:29 -0300

    feat: create api bot

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 import express from "express";
 import router from "./routes/index.js";
 const app = express();
+import cors from "cors";
 
 app.use(express.json());
 app.use("/calendar", router);
+app.use(cors());
 
 export { app };
